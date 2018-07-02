59c2a32760bd7188a5bab536e56c297e300df085
Radka Marty <martyradka@gmail.com>
2018-07-02 17:15:29 +0200

    changes in blog/; add requirements

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,3 @@ urlpatterns = [
     # jmeno stranky z view
     url(r'blog', views.post_list, name='post_list'),
 ]
-
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,10 @@
-# from django.shortcuts import render, get_object_or_404
-# from .models import Question
-#
-#
-# def post_list(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)  # helper f() to catch Http404 if the object doesn't exist
-#     return render(request, 'blog/post_list.html', {'question': question})
-
 from django.shortcuts import render
 from .models import Post
-from django.utils import timezone
 
 
 def post_list(request):
     """ this function will put together our template blog/post_list.html & data """
-    # filtruje vsechny publikovane posty starsiho casu, nez ted, takžze bude vlastne filtrovat vsechny
+    # filtruje vsechny publikovane posty starsiho casu, nez ted, takze bude vlastne filtrovat vsechny
     # lte - less that or equal to <=
     # lt - less than <
     # gte - greater than equal to >=
@@ -24,7 +15,3 @@ def post_list(request):
     # blog/post_list.html
     # {'posts': posts} = place in which we can add some things for the template to use
     return render(request, 'blog/post_list.html', {'posts': posts})
-
-
-
-
