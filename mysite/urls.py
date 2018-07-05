48acc6265704529f83584cc042e4aea227e98e99
Radka Marty <martyradka@gmail.com>
2018-07-05 16:55:13 +0200

    settings static url, typo fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from blog import views
 
 urlpatterns = [
-    url(r'^admin/$', admin.site.urls, name='admin'),
+    url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
